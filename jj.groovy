6493c89b107e402e3aa5defa8107f75536ca056f
dimaqp <dimaqp@gmail.com>
2021-02-28 15:37:23 +0200

    test1

--- jj.groovy
+++ jj.groovy
@@ -1,12 +1,37 @@
 #!groovy
-pipeline {
+peline {
     agent any
-
     stages {
-        stage('Hello') {
+//        stage('Prepare output folder ') {
+//            steps {
+//                del / q "C:\jMeter-Jenkins\apache-jmeter-5.2.1\bin\Execution_resultsTC1\*"
+//                FOR / D % % p IN("C:\jMeter-Jenkins\apache-jmeter-5.2.1\bin\Execution_resultsTC1\*.*") DO rmdir "%%p" / s / q
+//            }
+//        }
+        stage('TC_01') {
             steps {
-                echo 'Hello World'
+                echo 'This stage will be executed first.'
+                sh(script: "date -u")
+                build(job: 'TC1_Select a product from category')
             }
         }
+//        stage('Parallel Stage') {
+//            parallel {
+//                stage('TC_02') {
+//                    steps {
+//                        echo "TC_02"
+//                        sh(script: "date -u")
+//                        build(job: 'TC2_Write a review to a product')
+//                    }
+//                }
+//                stage('TC_03'){
+//                    steps{
+//                        echo "TC_03"
+//                        sh(script: "date -u")
+//                        build(job: 'TC3_Add and remove a product from Shopping Cart')
+//                    }
+//                }
+//            }
+//        }
     }
 }
